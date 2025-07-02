101da78d855f14a4cd3aeec3b1f747ff903b4adf
ayusclg <freelancerayus@gmail.com>
2025-07-02 14:19:21 +0545

    ApiError Handling

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,19 @@
+class ApiError extends Error{
+    statusCode: number;
+    success: boolean;
+    stack?: string;
+    
+    constructor(
+        statusCode:number,
+        message:string = "Something Went Wrong",
+        stack = ""
+
+    ) {
+        super(message)
+        this.statusCode = statusCode,
+            this.success = false,
+            this.name = "ApiError",
+         (stack) ?  this.stack =stack : Error.captureStackTrace(this,this.constructor)
+            
+    }
+}
