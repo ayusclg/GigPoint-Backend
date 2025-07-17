bc94a5eb5b0b832720666554eb07ac21362748fe
ayusclg <freelancerayus@gmail.com>
2025-07-18 00:40:41 +0545

    SendMail To User , minor fixes in google auth and cors origin change

--- src/controllers/googleController.ts
+++ src/controllers/googleController.ts
@@ -1,26 +1,27 @@
-import { asyncHandler } from "../utils/AsyncHandler"
-import { Request, Response } from "express"
-import { Iuser } from "../models/userModel"
-import { ApiError } from "../utils/ApiError"
-import { ApiResponse } from "../utils/ApiRes"
+import { asyncHandler } from "../utils/AsyncHandler";
+import { Request, Response } from "express";
+import { Iuser } from "../models/userModel";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiRes";
 
-
-const googleCallback = asyncHandler(async (req: Request, res: Response): Promise<void> => {
-    const user = req.user as Iuser
-    if(!user) throw new ApiError(403,"Permission Denied")
-    const refreshToken = user.generateRefreshToken()
-    const accessToken = user.generateAccessToken()
+const googleCallback = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const user = req.user as Iuser;
+    if (!user) throw new ApiError(403, "Permission Denied");
+    const refreshToken = user.generateRefreshToken();
+    const accessToken = user.generateAccessToken();
 
     res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        secure:false
-    })
+      httpOnly: true,
+      secure: false,
+    });
     res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure:false,
-    })
-     res.redirect("http://localhost:5173/dashboard?message=UserCreated");
-     res.status(201).json(new ApiResponse(200,user,"User Logged In"))
-})
+      httpOnly: true,
+      secure: false,
+    });
 
-export{googleCallback}
+    res.status(201).json(new ApiResponse(200, user, "User Logged In"));
+  }
+);
+
+export { googleCallback };
--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,12 @@ app.get("/", (req:Request, res:Response):void => {
  import "./config/Passport";
 //middlewares
 
-app.use(cors({
-    origin: "*",
-    credentials:true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 passport.initialize()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
