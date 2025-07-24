6c56c594189aea5e7abe9e3c5611e8cb1cbe9376
sudarshan <Mr.sudarshanuprety@gmail.com>
2025-07-24 21:50:31 +0545

    [improvement] using common model for resuable fields in database schema.

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,6 +2,9 @@ import mongoose, { Mongoose } from "mongoose";
 import jwt from 'jsonwebtoken'
 import { object } from "joi";
 
+import { createSchemaWithCommon, ICommon } from './commonModel';
+
+
 export interface Iuser extends Document{
     googleId: string;
     fullName: string,
@@ -23,6 +26,7 @@ export interface Iuser extends Document{
     isAvailable: boolean;
     resetOtp: string | undefined;
     resetOtpExpiry: Date | undefined;
+    passwordChangedAt: Date;
 }
 
 
