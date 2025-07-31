4ee02fe6843b92ef123b7390ea8ba4cfd73caa88
Saroj Simkhada <129253853+saroz-Xplore@users.noreply.github.com>
2025-07-31 13:16:38 +0545

    Update userController.ts (Paassword)

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -211,9 +211,10 @@ const updateWorkerDetails = asyncHandler(
       if (!checkPassword)
         throw new ApiError(403, "Please Enter Correct Current Password");
     }
-   
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-
+   if(newPassword)
+   {   
+     const hashedPassword = await bcrypt.hash(newPassword, 10);
+   }
     let cloudUrl;
     if (req.file) {
       const file = req.file as Express.Multer.File;
