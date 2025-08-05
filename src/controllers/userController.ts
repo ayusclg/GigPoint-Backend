93a01053e733acb900d8c92e29cf78364b5e7441
Aayushh <freelancerayus@gmail.com>
2025-08-05 23:29:16 +0545

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -107,12 +107,14 @@ const workerLogin = asyncHandler(
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "none" 
+      sameSite: "none" ,
+      path:"/api"
     });
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "none" 
+      sameSite: "none" ,
+      path:"/api"
     });
     res
       .status(200)
@@ -131,12 +133,14 @@ const userLogout = asyncHandler(
     res.clearCookie("refreshToken", {
       httpOnly: true,
       secure: true,
-      sameSite: "none" 
+      sameSite: "none" ,
+      path:"/api"
     });
     res.clearCookie("accessToken", {
       httpOnly: true,
       secure: true,
-      sameSite: "none" 
+      sameSite: "none" ,
+      path:"/api"
     });
     res.status(200).json(new ApiResponse(200, " ", "User logged Out"));
   }
