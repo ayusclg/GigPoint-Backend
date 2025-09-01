a1eea7816a856501afa650f896ed9d28b35eb69f
ayusclg <freelancerayus@gmail.com>
2025-09-01 19:20:19 +0545

    fix route

--- src/controllers/googleController.ts
+++ src/controllers/googleController.ts
@@ -15,14 +15,14 @@ const googleCallback = asyncHandler(
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true,
-      path: "/api",
+      path: "/",
       sameSite:"none"
     });
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      path:"/api"
+      path:"/"
     });
     res.redirect("http://localhost:5173/user-dashboard");
 
