a69dec966afc85cc1720576641bbed615d4ab5fd
ayusclg <freelancerayus@gmail.com>
2025-09-11 20:28:17 +0545

    Dashboard Admin

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -7,5 +7,6 @@ const router = express.Router()
 router.route("/viewAll").get(verifyUser,admin.viewAllUser)
 router.route("/removeUser").delete(verifyUser, admin.removeUser)
 router.route("/jobs").get(verifyUser, admin.jobList)
+router.route("/dash").get(verifyUser,admin.dashboardData)
 
 export default router
