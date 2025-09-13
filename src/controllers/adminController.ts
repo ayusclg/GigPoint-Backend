771244f18b940df4fc699e29252bbf6d83448c8f
ayusclg <freelancerayus@gmail.com>
2025-09-13 15:12:33 +0545

    Removinf uSER AND job post by admin

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -2,13 +2,21 @@ import { filterQuery } from "../middlewares/filterQuery";
 import { User } from "../models/userModel";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiRes";
-import { asyncHandler } from "../utils/AsyncHandler"
-import { Request,Response } from "express"
+import { asyncHandler } from "../utils/AsyncHandler";
+import { Request, Response } from "express";
 import { isAdmin } from "../utils/Rolecheck";
 import { Job } from "../models/jobModel";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import { Admin } from "mongodb";
+import { sendMail } from "../services/Nodemailer";
+import path from "path";
+import fs from "fs";
 
+const removeUserHTML = path.join(__dirname, "../templates/removeUser.html");
+const removeUserTemplate = fs.readFileSync(removeUserHTML, "utf-8");
+
+const removeJobHTML = path.join(__dirname, "../templates/removeJob.html")
+const removeJobTemplate = fs.readFileSync(removeJobHTML, "utf-8");
 class adminController {
   viewAllUser = asyncHandler(
     async (req: Request, res: Response): Promise<void> => {
@@ -51,14 +59,39 @@ class adminController {
       }
 
       const userId = req.params.id;
+      const message = req.body;
 
       const removeUser = await User.findById(userId);
       if (!removeUser || isAdmin(removeUser)) {
         throw new ApiError(404, "User CouldNot Be Deleted");
       }
-
-      await User.findByIdAndDelete(userId);
-
+      if (isAdmin(removeUser)) {
+        throw new ApiError(400, "Bad Request Cannot Delete Admin");
+      }
+      const userMail = removeUser.email;
+      const userName = removeUser.fullName;
+      const html = removeUserTemplate
+        .replace("{{username}}", userName)
+        .replace(
+          "{{body}}",
+          message ||
+            "we are sorry for this convinence that arised due to your inappropriate use of the platform."
+        );
+      const deleteUser = await User.findByIdAndDelete(userId);
+      if (deleteUser) {
+        const mailOptions = {
+          to: userMail,
+          subject: "You Are Restricted To Use GigPoint",
+          message: "You Are Restricted To Use GigPoint",
+          html,
+        };
+        const mailRes = await sendMail(mailOptions);
+        if (!mailRes) {
+          throw new ApiError(400, "Error In Sending Mail");
+        }
+      } else {
+        throw new ApiError(400, "Error In Deleting User");
+      }
       res
         .status(200)
         .json(new ApiResponse(200, removeUser, "User Successfully Deleted"));
@@ -99,6 +132,44 @@ class adminController {
     res.status(200).json(new ApiResponse(200, response, "Job List Fetched"));
   });
 
+  removeJob = asyncHandler(async (req: Request, res: Response): Promise<void> => {
+    const userCheck = await User.findById(req.userId) 
+    if (!userCheck || !isAdmin(userCheck)) {
+      throw new ApiError(403,"Permission Denied")
+    }
+     
+    const jobId = req.params.id;
+    const message = req.body;
+
+    const findJob = await Job.findById(jobId).populate("createdBy", "fullName email")
+    if (!findJob) {
+      throw new ApiError(404,"Job Not Valid")
+    }
+    const email = (findJob.createdBy as any).email;
+    const name = (findJob.createdBy as any).fullName;
+    const html = removeJobTemplate.replace("{{username}}", name).replace("{{body}}", message || "Your job post was removed because it did not comply with GigPoint community guidelines and posting policies.")
+    const deleteJob = await Job.findByIdAndDelete(findJob._id)
+    if (deleteJob) {
+      const mailOptions = {
+        to: email,
+        subject: "Your Job Posting Has Been Removed",
+        message: "Your Job Posting Has Been Removed",
+        html,
+      };
+      const send = await sendMail(mailOptions)
+      if (!send) {
+        throw new ApiError(400,"Mail Sending Error")
+      }
+    }
+    else {
+      throw new ApiError(400,"Error In Deleting Post")
+    }
+
+    res.status(200).json(new ApiResponse(200,findJob,"Job Has Been Removed"))
+    
+    
+  })
+
   seeTotalTransaction = asyncHandler(
     async (req: Request, res: Response): Promise<void> => {
       const userCheck = await User.findById(req.userId);
@@ -109,56 +180,60 @@ class adminController {
       const filter = req.query.filter as string;
     }
   );
-    
- 
-    dashboardData = asyncHandler(async (req: Request, res: Response) => {
-        const userCheck = await User.findById(req.userId)
-        if (!userCheck || !isAdmin(userCheck)) {
-            throw new ApiError(403, "Permission Denied")
-        }
-      const totalJobs = await Job.countDocuments()
-      if (totalJobs === 0) {
-        throw new ApiError(404,"No Jobs Found")
-      }
-      const activeJobs = await Job.countDocuments({ status: "searching" })
-      const ongoingJobs = await Job.countDocuments({ status: "assigned" })
-      const totalTransaction = await Job.aggregate([
-        {
-          $group: {
+
+  dashboardData = asyncHandler(async (req: Request, res: Response) => {
+    const userCheck = await User.findById(req.userId);
+    if (!userCheck || !isAdmin(userCheck)) {
+      throw new ApiError(403, "Permission Denied");
+    }
+    const totalJobs = await Job.countDocuments();
+    if (totalJobs === 0) {
+      throw new ApiError(404, "No Jobs Found");
+    }
+    const activeJobs = await Job.countDocuments({ status: "searching" });
+    const ongoingJobs = await Job.countDocuments({ status: "assigned" });
+    const totalTransaction = await Job.aggregate([
+      {
+        $group: {
           _id: null,
-          totalTransactions:{$sum:"$finalPrice"}
-        }}
-      ])
+          totalTransactions: { $sum: "$finalPrice" },
+        },
+      },
+    ]);
 
-      const totalUser = await User.countDocuments()
-      const totalAdmin = await User.countDocuments({role:"admin"})
-      const totalCustomers= await User.countDocuments({role:"user"})
-      const totalWorker = await User.countDocuments({role:"worker"})
-      if (totalUser === 0 || totalAdmin === 0 || totalCustomers === 0 || totalWorker === 0) {
-        throw new ApiError(404,"No Data Found")
-      }
+    const totalUser = await User.countDocuments();
+    const totalAdmin = await User.countDocuments({ role: "admin" });
+    const totalCustomers = await User.countDocuments({ role: "user" });
+    const totalWorker = await User.countDocuments({ role: "worker" });
+    if (
+      totalUser === 0 ||
+      totalAdmin === 0 ||
+      totalCustomers === 0 ||
+      totalWorker === 0
+    ) {
+      throw new ApiError(404, "No Data Found");
+    }
 
-      const JobsResponse = {
-        totalTransaction: totalTransaction[0].totalTransactions,
-        totalJobs,
-        activeJobs,
-        ongoingJobs,
-      }
-      const UserResponse = {
-        totalUser,
-        totalAdmin,
-        totalWorker,
-        totalCustomers,
-      }
-      const response = {
-        ...JobsResponse,
-        ...UserResponse
-      }
+    const JobsResponse = {
+      totalTransaction: totalTransaction[0].totalTransactions,
+      totalJobs,
+      activeJobs,
+      ongoingJobs,
+    };
+    const UserResponse = {
+      totalUser,
+      totalAdmin,
+      totalWorker,
+      totalCustomers,
+    };
+    const response = {
+      ...JobsResponse,
+      ...UserResponse,
+    };
 
-      res.status(200).json(new ApiResponse(200,response,"Dashboard Data Fetched"))
-    })
-   
-  
-
+    res
+      .status(200)
+      .json(new ApiResponse(200, response, "Dashboard Data Fetched"));
+  });
 }
-export const admin = new adminController()
+export const admin = new adminController();
